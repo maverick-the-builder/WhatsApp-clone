2c3c2ef05b46fdab8a38f0214ab96a115c3f970b
Anup Rawat <anuprawat077@gmail.com>
2024-03-10 10:54:28 +0530

    Initial Commit

---
+++ src/components/About.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h2>About Us</h2>
+      <p>Learn more about vite react....</p>
+      <p>bye...</p>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Welcome to the Home Page</h2>
+      <p>create-vite is a tool to quickly start a project from a basic template for popular frameworks...</p>
+    </div>
+  );
+};
+
+export default Home;
+
